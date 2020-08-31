8a6dfd4cb203dda48ef553678894f6c38eb342d9
langet <langet@epicea26.iut-infobio.priv.univ-lille1.fr>
2020-08-31 16:37:56 +0200

    Planete

--- src/crew/Planets.java
+++ src/crew/Planets.java
@@ -1,7 +1,24 @@
 package crew;
 
 public class Planets extends Entities {
+	private int ressources;
+	private String name;
 	
+	
+	public int getRessources() {
+		return ressources;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public Planets(int ressources, String name) {
+		super();
+		this.ressources = ressources;
+		this.name = name;
+	}
+
 	public Planets() {
 		
 	}
