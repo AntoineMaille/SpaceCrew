dc7bbd730b4b598f7a7a2bb15c7236e90c68ffaf
Lucas Delestree <delestrl@epicea23.iut-infobio.priv.univ-lille1.fr>
2020-08-31 16:21:54 +0200

    vaisseau

--- src/crew/Vaisseau.java
+++ src/crew/Vaisseau.java
@@ -1,10 +1,55 @@
 package crew;
 
-public class Vaisseau extends Entities {
-
+public class Vaisseau {
 	
-	public VaisseauType type;
-	public int vie;
-	//je suis un test
+	private VaisseauType type;
+	private int hp;
+	private int x;
+	private int y;
+	
+	
+	public Vaisseau(VaisseauType t,int v,int x,int y) {
+		this.type=t;
+		this.hp=v;
+		this.x=x;
+		this.y=y;
+	}
+
+
+	public int getVie() {
+		return hp;
+	}
+
+
+	public void setVie(int vie) {
+		this.hp = vie;
+	}
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
+	public VaisseauType getType() {
+		return type;
+	}
+	
+	
 	
 }
