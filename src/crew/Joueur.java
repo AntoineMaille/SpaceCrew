3b6dbd87e8dafdd962378b77cfbc59b6e6cf0709
langet <langet@epicea26.iut-infobio.priv.univ-lille1.fr>
2020-08-31 15:07:31 +0200

    joueur

---
+++ src/crew/Joueur.java
@@ -0,0 +1,40 @@
+package crew;
+
+public class Joueur {
+	private int numero;
+	private String name;
+	private CROISEUR v1;
+	private DESTROYER v2;
+	private CHASSEUR v3;
+	private ECLAIREUR v4;
+	
+	public int getNumero() {
+		return numero;
+	}
+	public String getName() {
+		return name;
+	}
+	public CROISEUR getV1() {
+		return v1;
+	}
+	public DESTROYER getV2() {
+		return v2;
+	}
+	public CHASSEUR getV3() {
+		return v3;
+	}
+	public ECLAIREUR getV4() {
+		return v4;
+	}
+	public Joueur(int numero, String name, CROISEUR v1, DESTROYER v2, CHASSEUR v3, ECLAIREUR v4) {
+		super();
+		this.numero = numero;
+		this.name = name;
+		this.v1 = v1;
+		this.v2 = v2;
+		this.v3 = v3;
+		this.v4 = v4;
+	}
+	
+	
+}
