cfbe3beaadcde5d773210293e364f6e73aa9ce2b
langet <langet@epicea26.iut-infobio.priv.univ-lille1.fr>
2020-08-31 16:18:10 +0200

    joueur

--- src/crew/Joueur.java
+++ src/crew/Joueur.java
@@ -3,11 +3,12 @@ package crew;
 public class Joueur {
 	private int numero;
 	private String name;
-	private VaisseauType Croiseur;
-	private VaisseauType Destroyer;
-	private VaisseauType Chasseur;
-	private VaisseauType Eclaireur;
+	private Vaisseau Croiseur;
+	private Vaisseau Destroyer;
+	private Vaisseau Chasseur;
+	private Vaisseau Eclaireur;
 	private int ressources;
+	private PlaneteJoueur Camp;
 	
 	public int getNumero() {
 		return numero;
@@ -15,23 +16,27 @@ public class Joueur {
 	public String getName() {
 		return name;
 	}
-	public VaisseauType getCroiseur() {
+	public Vaisseau getCroiseur() {
 		return Croiseur;
 	}
-	public VaisseauType getDestroyer() {
+	public Vaisseau getDestroyer() {
 		return Destroyer;
 	}
-	public VaisseauType getChasseur() {
+	public Vaisseau getChasseur() {
 		return Chasseur;
 	}
-	public VaisseauType getEclaireur() {
+	public Vaisseau getEclaireur() {
 		return Eclaireur;
 	}
 	public int getRessources() {
 		return ressources;
 	}
-	public Joueur(int numero, String name, VaisseauType croiseur, VaisseauType destroyer, VaisseauType chasseur,
-			VaisseauType eclaireur, int ressources) {
+	public PlaneteJoueur getCamp() {
+
+		return Camp;
+	}
+	public Joueur(int numero, String name, Vaisseau croiseur, Vaisseau destroyer, Vaisseau chasseur, Vaisseau eclaireur,
+			int ressources, PlaneteJoueur camp) {
 		super();
 		this.numero = numero;
 		this.name = name;
@@ -40,5 +45,8 @@ public class Joueur {
 		Chasseur = chasseur;
 		Eclaireur = eclaireur;
 		this.ressources = ressources;
+		Camp = camp;
 	}
+	
+	
 }
