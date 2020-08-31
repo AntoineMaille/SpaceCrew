665a93f44f4acceb16badf9314e9cab5fb63371e
Antoine Maille <antoine.maille.etu@univ-lille.fr>
2020-08-31 16:22:27 +0200

    Un magnifique affichage pour la map

--- src/crew/Map.java
+++ src/crew/Map.java
@@ -15,35 +15,30 @@ public class Map {
 		planetesJoueurs.add(new PlaneteJoueur());
 		planetesJoueurs.add(new PlaneteJoueur());
 		map = new Entities[Map.length][Map.length];
-		map [0] [0] = new PlaneteJoueur();
-		map [Map.length - 1][Map.length - 1] = new PlaneteJoueur();
+		map [0] [0] = planetesJoueurs.get(0);
+		map [Map.length - 1][Map.length - 1] = planetesJoueurs.get(1);
 	}
 
 	public String toString() {
 		StringBuilder res = new StringBuilder();
-		for (int bordure = 0; bordure < map.length * 3; bordure++) {
-			res.append("_");
-		}
 		for (int ligne = 0; ligne < Map.length ; ligne++) {
 			res.append('\n');
-			for (int colonne = 0; colonne < Map.length; colonne++) {
-				if(ligne % 2 ==  0 ) {
-					if(colonne % 2 == 1)
-						res.append("__");
-					else
-						res.append(" ");
+			for (int separation = 0; separation < map.length; separation++) {
+				res.append(" ---");
 				}
-				else if(ligne % 2 == 1) {
-					if(colonne % 2 == 1	)
-						res.append('|');
-					else
-						res.append(' ');
+			res.append('\n');
+			for (int colonne = 0; colonne < Map.length; colonne++) {
+				if( map[ligne] [colonne] instanceof Planets || map[ligne][colonne] instanceof Vaisseau)
+					res.append("| " + map[ligne][colonne].toString() + " ");
+				else {
+					res.append("|   ");
 				}
 			}
+			res.append("|");
 		}
 		return res.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Map map = new Map();
 		System.out.println(map);
