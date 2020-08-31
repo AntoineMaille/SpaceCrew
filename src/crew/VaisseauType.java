ca376a79dc60b135e6b4936e5b702df87268705c
houtekis <houtekis@epicea24.iut-infobio.priv.univ-lille1.fr>
2020-08-31 14:58:33 +0200

    VaisseauType

--- src/crew/VaisseauType.java
+++ src/crew/VaisseauType.java
@@ -1,7 +1,7 @@
 package crew;
 
 public enum VaisseauType {
-	CROISEUR('C','500','20','2'),DESTROYER('D','300','60','2'),CHASSEUR('H','200','35','3'),ECLAIREUR('E','100','25','5');
+	CROISEUR('C',500,20,2),DESTROYER('D',300,60,2),CHASSEUR('H',200,35,3),ECLAIREUR('E',100,25,5);
 		private char l;
     	private int vie;
 		private int attaque;
@@ -13,5 +13,23 @@ public enum VaisseauType {
 			this.attaque = attaque;
 			this.movementPoint = movementPoint;
 		}
+
+		public char getL() {
+			return l;
+		}
+
+		public int getVie() {
+			return vie;
+		}
+
+		public int getAttaque() {
+			return attaque;
+		}
+
+		public int getMovementPoint() {
+			return movementPoint;
+		}
+		
+		
 	
 }
