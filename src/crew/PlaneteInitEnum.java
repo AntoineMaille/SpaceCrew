8eff447ae96b821612a82217953b3f13743baef4
Lucas Delestree <lucas.delestree.etu@univ-lille.fr>
2020-09-01 09:02:39 +0200

    Add new file

---
+++ src/crew/PlaneteInitEnum.java
@@ -0,0 +1,20 @@
+package crew;
+
+public enum PlaneteInitEnum {
+	PlanNO(0,0),PlanNE(0,19),PlanSO(19,0),PlanSE(19,19);
+	private int x;
+	private int y;
+	
+	private PlaneteInitEnum(int i,int j) {
+		this.x=i;
+		this.y=j;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	public int getY() {
+		return this.y;
+	}
+	
+}
