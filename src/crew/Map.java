174517f24ae1b40f16409bf802c50dbe643c7340
houtekis <houtekis@epicea24.iut-infobio.priv.univ-lille1.fr>
2020-09-01 09:24:39 +0200

    Mise à jour de la map

--- src/crew/Map.java
+++ src/crew/Map.java
@@ -6,17 +6,28 @@ import java.util.Random;
 public class Map {
 	private ArrayList<Planets> planetesJoueurs;
 	private ArrayList<Planets> planetesRandom;
-	private static Random ran;
 	private Entities [][] map;
 	private static int length = 20;
+	private int compteurPlanete = -1;
 
 	public Map() {
 		planetesJoueurs = new ArrayList<Planets>();
+		planetesRandom = new ArrayList<Planets>();
 		planetesJoueurs.add(new PlaneteJoueur());
 		planetesJoueurs.add(new PlaneteJoueur());
 		map = new Entities[Map.length][Map.length];
 		map [0] [0] = planetesJoueurs.get(0);
 		map [Map.length - 1][Map.length - 1] = planetesJoueurs.get(1);
+		for (int i = 0; i < (Map.length); i++) {
+			for (int j = 0; j < (Map.length); j++) {
+				if((((int) (Math.random() * 15))  >= 14) && (!((i > -1 && i < 4)&& (j > -1 && j < 4))) && (!((i > Map.length - 5 && i < Map.length) && ( j > Map.length - 5 && j < Map.length))) && (!((i > (Map.length/2 - 3) && i < (Map.length/2 + 2)) && ( j > (Map.length/2 - 3) && j < (Map.length/2 + 2))))) {
+					this.planetesRandom.add(new PlaneteRandom());
+					compteurPlanete++;
+					planetesRandom.add(new PlaneteRandom());					
+					map[i][j] = planetesRandom.get(compteurPlanete);
+				}
+			}
+		}
 	}
 
 	public String toString() {
@@ -28,6 +39,7 @@ public class Map {
 				}
 			res.append('\n');
 			for (int colonne = 0; colonne < Map.length; colonne++) {
+				
 				if( map[ligne] [colonne] instanceof Planets || map[ligne][colonne] instanceof Vaisseau)
 					res.append("| " + map[ligne][colonne].toString() + " ");
 				else {
@@ -39,7 +51,8 @@ public class Map {
 		}
 		return res.toString();
 	}
-
+	
+	
 	public static void main(String[] args) {
 		Map map = new Map();
 		System.out.println(map);
