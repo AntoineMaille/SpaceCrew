b4345bb0645c7289b93406a7dd535f3588858ade
Lucas Delestree <lucas.delestree.etu@univ-lille.fr>
2020-09-01 11:26:18 +0200

    Update VaisseauType.java

--- src/crew/VaisseauType.java
+++ src/crew/VaisseauType.java
@@ -1,20 +1,20 @@
 package crew;
 
 public enum VaisseauType {
-	CROISEUR('C',500,20,2),DESTROYER('D',300,60,2),CHASSEUR('H',200,35,3),ECLAIREUR('E',100,25,5);
-		private char l;
+	CROISEUR("C",500,20,2),DESTROYER("D",300,60,2),CHASSEUR("H",200,35,3),ECLAIREUR("E",100,25,5);
+		private String l;
     	private int vie;
 		private int attaque;
 		private int movementPoint;
 		
-		private VaisseauType(char l, int vie, int attaque, int movementPoint) {
+		private VaisseauType(String l, int vie, int attaque, int movementPoint) {
 			this.l= l;
 			this.vie = vie;
 			this.attaque = attaque;
 			this.movementPoint = movementPoint;
 		}
 
-		public char getL() {
+		public String getL() {
 			return l;
 		}
 
