5e85947fbf30f0d5e528f0091899770f366fccd5
Lucas Delestree <lucas.delestree.etu@univ-lille.fr>
2020-09-02 11:36:09 +0200

    noms planetes

--- src/crew/PlaneteRandom.java
+++ src/crew/PlaneteRandom.java
@@ -7,9 +7,10 @@ public class PlaneteRandom extends Planets{
 	private int recharge;
 	private static final int tempsRecharge = 3;
 	private static Random ran = new Random();
+	private static String[] nPlanete=new String[] {"Xanax","Kraz","Andorres","Mustafir","Tatouin","Corussynthe","Antares","Betelgieuse","Titawin","Dabogat","Igor","Géonosis","Colth"};
 	
 	public PlaneteRandom(int x, int y) {
-		super((ran.nextInt(1000 - 300) + 1) + 300, "toto", x, y);
+		super((ran.nextInt(1000 - 300) + 1) + 300, nPlanete[(int)(Math.random()* 13)], x, y);
 		this.currentVessel = null;
 		this.recharge = 5;
 	}
